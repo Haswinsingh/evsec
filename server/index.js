eb4c6ae332f29b7fb279eec733927e26000d88a5
Haswinsingh <haswinsinghak@gmail.com>
2026-09-02 14:40:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "https://evsec-nine.vercel.app/",
+        origin: "https://evsec-nine.vercel.app",
         methods: ["GET", "POST"]
     }
 });
